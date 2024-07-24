c44c443a640f4cfb0978fdd7e75bfdb800e8f17f
aminravaei10 <amin.ravaei10@gmail.com>
2024-07-24 10:54:04 +0330

    nest doc controller, provider, module and middleware

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  // @HttpCode(204)
+  // @Header('Cache-Control', 'none')
+  create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
   @Get()
-  findAll(): string {
-    return 'this action return all cats';
+  findAll() {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return id;
   }
 }
